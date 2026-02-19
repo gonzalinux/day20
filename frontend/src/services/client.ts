b728d7b693897a0575885186467223b00037a103
gonzalinux <gonzaleonrey@hotmail.com>
2026-02-19 17:23:50 +0100

    Mini fix

--- frontend/src/services/client.ts
+++ frontend/src/services/client.ts
@@ -1,8 +1,8 @@
-import { treaty } from "@elysiajs/eden";
-import type { App } from "../../../backend";
+import { treaty } from '@elysiajs/eden'
+import type { App } from '../../../backend'
 
-const baseUrl = import.meta.env.DEV ? "localhost:3000" : window.location.host;
+const baseUrl = import.meta.env.DEV ? 'localhost:3000' : window.location.host
 
-export const api = treaty<App>(baseUrl, {
-  fetch: { credentials: "include" },
-});
+export const api = treaty<App>(baseUrl + '/v1/api', {
+  fetch: { credentials: 'include' },
+})
